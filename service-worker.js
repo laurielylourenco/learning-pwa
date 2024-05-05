123ba4abfe25e65766eab8a306c969fb7457fd18
lauriely lourenço <49760702+laurielylourenco@users.noreply.github.com>
2024-05-05 18:14:16 +0000

    feat: pwa

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ self.addEventListener('install', function(event) {
         return cache.addAll([
           '/',
           '/index.html',
-          '/manifest.json',
+          '/https://laurielylourenco.github.io/learning-pwa/.json',
           '/free-wi.png'
         ]);
       })
